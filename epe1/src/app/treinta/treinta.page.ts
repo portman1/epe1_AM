a31e1462ed1760481ec5ea994f8b1c475b869096
portman1 <38507372+portman1@users.noreply.github.com>
2019-04-07 00:36:30 -0400

    xd

---
+++ epe1/src/app/treinta/treinta.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-treinta',
+  templateUrl: './treinta.page.html',
+  styleUrls: ['./treinta.page.scss'],
+})
+export class TreintaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
